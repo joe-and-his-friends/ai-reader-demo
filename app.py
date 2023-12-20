e5395a9518a0d1e5ef245e48f3fa99d569ac3a89
万康 <407351071@qq.com>
2023-12-20 15:49:50 +0800

    PDF and YouTube 視頻的文字讀取器，與文字分析AI對話系統

---
+++ app.py
@@ -0,0 +1,138 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+
+from video import get_text_from_youtube_video_url
+
+# 使用PDF读取器，按页读取内容
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    print(text)
+    return text
+
+# 文本分块
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+# 文字矢量入库
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+# 交谈历史链
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+# 处理用户输入问题
+def handle_user_input(user_question):
+    if user_question:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
+
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+    else:
+        print('user_question is null')
+
+def into_store(raw_text):
+    # get the text chunks
+    text_chunks = get_text_chunks(raw_text)
+
+    # for c in text_chunks:
+    #     print("\033[91m" + c + "\033[0m")
+    
+    # create vector store
+    vectorstore = get_vectorstore(text_chunks)
+
+    # create conversation chain
+    st.session_state.conversation = get_conversation_chain(vectorstore)
+
+# 项目入口
+def main():
+    load_dotenv()
+    title = "对话 with multiple 文件"
+    icon = ":books:"
+
+    # 设置html模版参数
+    st.set_page_config(page_title=title,page_icon=icon)
+    # 应用css
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    # 设置标题
+    st.header(title + icon)
+
+    # 接收用户输入
+    user_question = st.text_input("Ask a question about your files:")
+
+    # 回答，相当于user_question的on_change处理
+    if user_question:
+        handle_user_input(user_question)
+    
+    # with语句适用于对资源进行访问的场合，它可以确保在对资源的操作过程中不管是否发生异常都会自动执行释放资源的操作，相当于try catch
+    # 在sidebar里
+    with st.sidebar:
+        # 设置侧边栏标题
+        st.subheader("Your documents")
+        # 获取上传文件结果
+        pdf_docs = st.file_uploader("Upload your Files(PDF，Video) here and click on 'Process'", accept_multiple_files=True)
+        youtube_url = st.sidebar.text_area(label="What is the YouTube video URL?", max_chars=100 )
+
+        # 按钮点击，把传入文件分析入库
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                list_text=""
+                # 如果有PDF
+                if pdf_docs:
+                    # 这里可能反复上传同一个PDF，考虑做个字典
+                    list_text += get_pdf_text(pdf_docs)
+                # 如果有视频链接
+                if youtube_url:
+                    # 这里可能反复输入同一个地址，考虑做个字典
+                    list_text += get_text_from_youtube_video_url(youtube_url)
+
+                # 合并分析
+                into_store(list_text)
+            
+if __name__ == '__main__':
+    main()
